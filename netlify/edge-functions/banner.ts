faee29c4ac77ec7bc26b2fa97b503ddd6c1398e0
Noxdor <lennart@guderian.de>
2023-04-08 20:03:34 +0200

    Adds check for an api token to protect the api.

--- netlify/edge-functions/banner.ts
+++ netlify/edge-functions/banner.ts
@@ -1,22 +1,31 @@
-import { Redis } from "https://deno.land/x/upstash_redis@v1.3.2/mod.ts"
+import { Redis } from "https://deno.land/x/upstash_redis@v1.3.2/mod.ts";
 
 /** Name of the variable inside redis for keeping track of how many people clicked the banner. */
-const bannerCounter = "banner"
+const bannerCounter = "banner";
 
-export default async () => {
-  const redisUrl = Deno.env.get("UPSTASH_REDIS_REST_URL")
-  const redisToken = Deno.env.get("UPSTASH_REDIS_REST_TOKEN")
+export default async (req: Request) => {
+  const redisUrl = Deno.env.get("UPSTASH_REDIS_REST_URL");
+  const redisToken = Deno.env.get("UPSTASH_REDIS_REST_TOKEN");
+  const apiToken = Deno.env.get("API_TOKEN");
 
-  if (!redisUrl || !redisToken) return new Response("", { status: 500 })
+  if (!redisUrl || !redisToken || !apiToken) {
+    return new Response("", { status: 500 });
+  }
+
+  // return unauthorized of no api token in request
+  if (req.headers.get("X-Api-Token") !== apiToken) {
+    return new Response("", { status: 401 });
+  }
+
   const redis = new Redis({
     url: redisUrl,
     token: redisToken,
-  })
+  });
   try {
-    await redis.incr(bannerCounter)
+    await redis.incr(bannerCounter);
   } catch {
-    return new Response("", { status: 500 })
+    return new Response("", { status: 500 });
   }
-  return new Response()
-}
-export const config = { path: "/api/banner" }
+  return new Response();
+};
+export const config = { path: "/api/banner" };

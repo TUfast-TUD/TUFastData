9175592f692c96488ebca687eed15dfbe113c762
Noxdor <lennart@guderian.de>
2023-04-08 17:53:40 +0200

    Working example counter inplementation

--- netlify/functions/hello-world.ts
+++ netlify/functions/hello-world.ts
@@ -1,13 +1,30 @@
 import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions"
+import { Redis } from "@upstash/redis"
 
 export const handler: Handler = async (
   _event: HandlerEvent,
   _context: HandlerContext
 ) => {
+  const redisUrl = process.env.UPSTASH_REDIS_REST_URL
+  const redisRestToken = process.env.UPSTASH_REDIS_REST_TOKEN
+  if (!redisUrl || !redisRestToken)
+    return {
+      statusCode: 500,
+      error:
+        "There was an error with the server configuration. \
+Please contact the TUfast-Team about this at frage@tu-fast.de",
+    }
+  const redis = new Redis({
+    url: redisUrl,
+    token: redisRestToken,
+  })
+  redis.incr("counter")
+  const newCounter = await redis.get("counter")
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: "Hello, world!",
+      counter: newCounter,
     }),
   }
 }
